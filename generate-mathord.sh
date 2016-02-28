b839790d7eca60b9c88e65cc837ffe062d0a5bcc
Darcy Hu <hot123tea123@gmail.com>
2016-02-28 23:20:02 +0800

    添加了脚本 generate-mathord.sh，生成常用数学符号对应的码表，输出文件为 mathord.html．

---
+++ generate-mathord.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# 生成常用数学符号对应的码表，这里包含了大量的数学符号
+# 输出文件为 mathord.html
+#
+sed '/mathord/!d' unicode-math-table.tex > mathord.html
+# 删掉与其他脚本生成结果重复的内容
+sed -i -e '/Bbb/d' -e '/mbf/d' -e '/mit/d' -e '/msans/d' -e '/mtt/d' mathord.html
+cut -f 1 -d"}" mathord.html > usv.tmp
+cut -f 2 -d'"' usv.tmp > tmpfile
+mv tmpfile usv.tmp
+sed -i 's/^/\&#x/' usv.tmp
+sed -i 's/$/;<br>/' usv.tmp
+
+cut -f3 -d"{" mathord.html > value.tmp
+sed -i 's/}//' value.tmp
+
+paste -d" " value.tmp usv.tmp > mathord.html
+
+# 最后的清理工作
+rm -f *.tmp
+
