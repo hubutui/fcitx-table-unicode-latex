2fc33a5240086dcfbfcdc140a82fe3a35fdd50fa
Darcy Hu <hot123tea123@gmail.com>
2016-02-29 23:50:00 +0800

    终于改完名字了．

--- generate-latin-upper-mtt.sh
+++ generate-latin-upper-mtt.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 # 从 unicode-math-table.tex 生成等宽的大写拉丁字母，也就是 \mathtt 命令的结果
-# 输出文件为 Latin-mtt.html
+# 输出文件为 latin-upper-mtt.html
 #
-sed '/mtt[A-Z] /!d' unicode-math-table.tex > Latin-mtt.html
-cut -f 1 -d"}" Latin-mtt.html > usv.tmp
+sed '/mtt[A-Z] /!d' unicode-math-table.tex > latin-upper-mtt.html
+cut -f 1 -d"}" latin-upper-mtt.html > usv.tmp
 cut -f 2 -d'"' usv.tmp > tmpfile
 mv tmpfile usv.tmp
 sed -i 's/^/\&#x/' usv.tmp
 sed -i 's/$/;<br>/' usv.tmp
 
-cut -f3 -d"{" Latin-mtt.html > value.tmp
+cut -f3 -d"{" latin-upper-mtt.html > value.tmp
 sed -i 's/}//' value.tmp
 
-paste -d" " value.tmp usv.tmp > Latin-mtt.html
+paste -d" " value.tmp usv.tmp > latin-upper-mtt.html
 
 # 最后的清理工作
 rm -f *.tmp
