4982384922cdf2ef82712b348df6a0b2564782ce
Darcy Hu <hot123tea123@gmail.com>
2016-02-29 23:21:35 +0800

    写清除了注释．

--- generate-greek-upper-mit.sh
+++ generate-greek-upper-mit.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # 由 unicode-math-table.tex 生成斜体的大写希腊字母对应的码表
+# 输出文件为 greek-upper-mit.html
 #
 sed -e '/mathalpha/!d' -e '/mit[A-Z][a-z]\|mitvar[A-Z]/!d' unicode-math-table.tex > greek-upper-mit.html
 cut -f 1 -d"}" greek-upper-mit.html > usv.tmp
