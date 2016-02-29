945dab1df1231df88280e7bf58223d0b05560b1f
Darcy Hu <hot123tea123@gmail.com>
2016-02-29 21:48:21 +0800

    原来是用 greek 和 Greek 来区分大小写，但是考虑到这样不够直观，该用 upper 和 lower 来区分更好．

---
+++ generate-greek-lower-upright.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# 由 unicode-math-table.tex 生成直立的小写希腊字母对应的码表
+# 输出文件为 greek-lower-upright.html
+#
+sed -e '/mathalpha/!d' -e '/[\]up[a-z][a-z]/!d' unicode-math-table.tex > greek-lower-upright.html
+cut -f 1 -d"}" greek-lower-upright.html > usv.tmp
+cut -f 2 -d'"' usv.tmp > tmpfile
+mv tmpfile usv.tmp
+sed -i 's/^/\&#x/' usv.tmp
+sed -i 's/$/;<br>/' usv.tmp
+
+cut -f3 -d"{" greek-lower-upright.html > value.tmp
+sed -i 's/}//' value.tmp
+sed -i 's/up//' value.tmp
+paste -d" " value.tmp usv.tmp > greek-lower-upright.html
+
+# 最后的清理工作
+rm -f *.tmp
