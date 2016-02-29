9f5e431d20e2d12d40ecd7f79c497ad1f8ede902
Darcy Hu <hot123tea123@gmail.com>
2016-02-29 23:17:32 +0800

    改名了；更新了正则表达式，匹配更加准确简单．

---
+++ generate-greek-upper-mit.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# 由 unicode-math-table.tex 生成斜体的大写希腊字母对应的码表
+#
+sed -e '/mathalpha/!d' -e '/mit[A-Z][a-z]\|mitvar[A-Z]/!d' unicode-math-table.tex > greek-upper-mit.html
+cut -f 1 -d"}" greek-upper-mit.html > usv.tmp
+cut -f 2 -d'"' usv.tmp > tmpfile
+mv tmpfile usv.tmp
+sed -i 's/^/\&#x/' usv.tmp
+sed -i 's/$/;<br>/' usv.tmp
+
+cut -f3 -d"{" greek-upper-mit.html > value.tmp
+sed -i 's/}//' value.tmp
+
+paste -d" " value.tmp usv.tmp > greek-upper-mit.html
+
+# 最后的清理工作
+rm -f *.tmp
+
