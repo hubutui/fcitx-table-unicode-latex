e0ce3399c858afa01e4823bc90c17c6ba8d1bd95
Darcy Hu <hot123tea123@gmail.com>
2016-02-29 23:47:23 +0800

    改名字咯；而且发现前面改错名字了．更囧

--- generate-latin-upper-mit.sh
+++ generate-latin-upper-mit.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 # 由 unicode-math-table.tex 生成斜体大写拉丁字母对应的码表
 #
-sed -n '/mit[A-Z] /p' unicode-math-table.tex > Latin-mit.html
-cut -f 1 -d"}" Latin-mit.html > usv.tmp
+sed -n '/mit[A-Z] /p' unicode-math-table.tex > latin-upper-mit.html
+cut -f 1 -d"}" latin-upper-mit.html > usv.tmp
 cut -f 2 -d'"' usv.tmp > tmpfile
 mv tmpfile usv.tmp
 sed -i 's/^/\&#x/' usv.tmp
 sed -i 's/$/;<br>/' usv.tmp
 
-cut -f3 -d"{" Latin-mit.html > value.tmp
+cut -f3 -d"{" latin-upper-mit.html > value.tmp
 sed -i 's/}//' value.tmp
 
-paste -d" " value.tmp usv.tmp > Latin-mit.html
+paste -d" " value.tmp usv.tmp > latin-upper-mit.html
 
 # 最后的清理工作
 rm -f *.tmp
