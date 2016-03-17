c25045445c64a55ce35c131d734e43f1e30afa8d
Darcy Hu <hot123tea123@gmail.com>
2016-03-18 01:33:15 +0800

    unicode-math-table.tex 将 \up 命令前缀改成了 \mup，于是．．．

--- generate-greek-lower-upright.sh
+++ generate-greek-lower-upright.sh
@@ -2,7 +2,7 @@
 # 由 unicode-math-table.tex 生成直立的小写希腊字母对应的码表
 # 输出文件为 greek-lower-upright.html
 #
-sed -e '/mathalpha/!d' -e '/[\]up[a-z][a-z]/!d' unicode-math-table.tex > greek-lower-upright.html
+sed -e '/mathalpha/!d' -e '/[\]mup[a-z][a-z]/!d' unicode-math-table.tex > greek-lower-upright.html
 cut -f 1 -d"}" greek-lower-upright.html > usv.tmp
 cut -f 2 -d'"' usv.tmp > tmpfile
 mv tmpfile usv.tmp
@@ -11,7 +11,7 @@ sed -i 's/$/;<br>/' usv.tmp
 
 cut -f3 -d"{" greek-lower-upright.html > value.tmp
 sed -i 's/}//' value.tmp
-sed -i 's/up//' value.tmp
+sed -i 's/mup//' value.tmp
 paste -d" " value.tmp usv.tmp > greek-lower-upright.html
 
 # 删除错误匹配的大写字母
