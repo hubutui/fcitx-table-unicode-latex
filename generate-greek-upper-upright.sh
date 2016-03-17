efa956f9ecc2d4ce76e65532ba46c49634d8981f
Darcy Hu <hot123tea123@gmail.com>
2016-03-18 01:37:17 +0800

    根据 unicode-math-table.tex 更新了正则表达式

--- generate-greek-upper-upright.sh
+++ generate-greek-upper-upright.sh
@@ -2,7 +2,7 @@
 # 由 unicode-math-table.tex 生成直立的大写希腊字母对应的码表
 # 输出文件为 greek-upper-upright.html
 #
-sed -e '/mathalpha/!d' -e '/[\]up[A-Z][a-z]\|[\]upvar[A-Z]/!d' unicode-math-table.tex > greek-upper-upright.html
+sed -e '/mathalpha/!d' -e '/[\]mup[A-Z][a-z]\|[\]mupvar[A-Z]/!d' unicode-math-table.tex > greek-upper-upright.html
 cut -f 1 -d"}" greek-upper-upright.html > usv.tmp
 cut -f 2 -d'"' usv.tmp > tmpfile
 mv tmpfile usv.tmp
@@ -11,7 +11,7 @@ sed -i 's/$/;<br>/' usv.tmp
 
 cut -f3 -d"{" greek-upper-upright.html > value.tmp
 sed -i 's/}//' value.tmp
-sed -i 's/up//' value.tmp
+sed -i 's/mup//' value.tmp
 paste -d" " value.tmp usv.tmp > greek-upper-upright.html
 
 # 最后的清理工作
